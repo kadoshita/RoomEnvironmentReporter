c7b30a48146f59612e29c0109ae804411159eca2
kadoshita <sublimer.me@gmail.com>
2021-07-04 18:27:11 +0900

    Add HTTP Server

--- RoomEnvironmentReporter.ino
+++ RoomEnvironmentReporter.ino
@@ -1,15 +1,25 @@
 #include <M5StickC.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
 
 
 
+WebServer server(80);
+
+void handleStatus()
+{
+  server.send(200, "application/json", "{\"status\":\"OK\"}");
+}
 void setup()
 {
+  pinMode(10, OUTPUT);
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
   {
     digitalWrite(10, HIGH);
   }
 
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED)
@@ -19,6 +29,10 @@ void setup()
     digitalWrite(10, LOW);
     delay(500);
   }
+  digitalWrite(10, LOW);
+
+  server.on("/status", handleStatus);
+  server.begin();
 
   M5.begin();
   M5.Lcd.print("Test");
@@ -26,5 +40,5 @@ void setup()
 
 void loop()
 {
-  delay(10 * 1000);
+  server.handleClient();
 }
